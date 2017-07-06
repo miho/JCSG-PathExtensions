910386456a78bd32b35641511468a76a2a2822fd
Michael Hoffer <info@michaelhoffer.de>
2017-07-06 21:28:46 +0200

    Nice feature added: profiles can be extruded along paths.

---
+++ src/main/java/eu/mihosoft/jcsg/ext/path/PathProfile.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.jcsg.ext.path;
+
+import eu.mihosoft.vvecmath.Vector3d;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Path profile. A profile consists of a center and a list of points which
+ * define the outline of the profile.
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+public final class PathProfile {
+
+    private final List<Vector3d> points = new ArrayList<>();
+    private final Vector3d center;
+
+    /**
+     * Constructor. Creates a new profile.
+     *
+     * @param center center of the profile
+     * @param points profile points (outline)
+     */
+    private PathProfile(Vector3d center, List<Vector3d> points) {
+        this.center = center;
+        this.points.addAll(points);
+    }
+
+    /**
+     * Constructor. Creates a new profile from the specified prototype (performs
+     * a deep copy).
+     *
+     * @param other profile prototype
+     */
+    public PathProfile(PathProfile other) {
+        this.center = other.center;
+        this.points.addAll(other.points.stream().map(point -> point.clone()).collect(Collectors.toList()));
+    }
+
+    public List<Vector3d> getPoints() {
+        return points;
+    }
+
+    public Vector3d getCenter() {
+        return center;
+    }
+
+    /**
+     * Creates a new profile from the specified points.
+     *
+     * @param center profie center
+     * @param points profile points
+     * @return new profile
+     */
+    public static PathProfile fromPoints(Vector3d center, List<Vector3d> points) {
+        return new PathProfile(center, points);
+    }
+}
