3648ae4a5827801960fef4e18b84be82f46ee77f
Michael Hoffer <info@michaelhoffer.de>
2017-07-14 16:38:08 +0800

    new extrudeprofile class added

--- src/main/java/eu/mihosoft/jcsg/ext/path/ExtrudeProfile.java
+++ src/main/java/eu/mihosoft/jcsg/ext/path/ExtrudeProfile.java
@@ -1,4 +1,26 @@
 /*
+ * Copyright 2017 Michael Hoffer <info@michaelhoffer.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * If you use this software for scientific research then please cite the following publication(s):
+ *
+ * M. Hoffer, C. Poliwoda, & G. Wittum. (2013). Visual reflection library:
+ * a framework for declarative GUI programming on the Java platform.
+ * Computing and Visualization in Science, 2013, 16(4),
+ * 181–192. http://doi.org/10.1007/s00791-014-0230-y
+ */
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,8 +30,6 @@ package eu.mihosoft.jcsg.ext.path;
 import eu.mihosoft.jcsg.CSG;
 import eu.mihosoft.jcsg.Extrude;
 import eu.mihosoft.jcsg.Polygon;
-import eu.mihosoft.vvecmath.ModifiableVector3d;
-import eu.mihosoft.vvecmath.Plane;
 import eu.mihosoft.vvecmath.Transform;
 import eu.mihosoft.vvecmath.Vector3d;
 import java.util.ArrayList;
